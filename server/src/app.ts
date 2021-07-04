349aadc8c0a36cd403348da99c8d22045c3231f9
DONG WOONG OH <integral5137@gmail.com>
2021-07-04 23:08:57 +0900

    Client Set up

---
+++ server/src/app.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import cors from "cors";
+import express, { Express } from "express";
+import todoRouters from "./routes";
+
+const app: Express = express();
+const PORT: string | number = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(todoRouters);
+
+const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose.set("useFindAndModify", false);
+
+mongoose
+  .connect(uri, options)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT} 🚀`)
+    )
+  )
+  .catch((error) => {
+    throw error;
+  });
